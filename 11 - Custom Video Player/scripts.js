2dc70cf0e0b2209d7328a5ebd40cf134192fd6e3
Ruka Okuyama <rukaokuyama@gmail.com>
2023-04-02 20:50:57 +0900

    added skip function

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -13,11 +13,16 @@ function togglePlay() {
 }
 
 function updateButton() {
-  const icon = this.pause ? '►' : '❚ ❚'
+  const icon = this.paused ? '►' : '❚ ❚'
   toggle.textContent = icon;
+}
+
+function skip() {
+  video.currentTime += parseFloat(this.dataset.skip);
 }
 
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton)
 toggle.addEventListener('click', togglePlay);
+skipButtons.forEach(button => button.addEventListener('click', skip));
