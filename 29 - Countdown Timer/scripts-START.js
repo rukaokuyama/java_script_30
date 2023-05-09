f2cae161eb7eb3659f4a5bf0bc25d564d6cf1d54
Ruka Okuyama <rukaokuyama@gmail.com>
2023-05-09 14:26:21 +0900

    displaying time

--- 29 - Countdown Timer/scripts-START.js
+++ 29 - Countdown Timer/scripts-START.js
@@ -1,8 +1,13 @@
 let countdown;
+const timerDisplay = document.querySelector('.display__time-left');
+const endTime = document.querySelector('.display__end-time"');
+const buttons = document.querySelectorAll('[data-time]');
 
 function timer(seconds) {
   const now = Date.now();
   const then = now + seconds * 1000;
+  displayTimeLeft(seconds);
+  displayEndTime(then);
 
   countdown = setInterval(() => {
     const secondsLeft = Math.round((then - Date.now()) / 1000);
@@ -16,5 +21,23 @@ function timer(seconds) {
 }
 
 function displayTimeLeft(seconds) {
-
+const minutes = Math.floor(seconds / 60);
+const remainderSeconds = seconds % 60;
+const display = `${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`;
+document.title = display;
+timerDisplay.textContent = display;
 }
+
+function displayEndTime(timestamp) {
+  const end = new Date(timestamp);
+  const hour = end.getHours();
+  const minutes = end.getMinutes();
+  endTime.textContent = `Be Back At ${hour > 12 ? hours - 12 : hour}:${minutes < 10 ? '0' : ''}${minutes}`;
+}
+
+function startTimer() {
+  const seconds = parseInt(this.dataset.time);
+  timer(seconds);
+}
+
+buttons.forEach(button => button.addEventListener('click', startTimer));
