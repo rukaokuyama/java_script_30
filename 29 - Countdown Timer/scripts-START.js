56bb08e8f998bde9b54c4f512fb52cec1796a0c3
Ruka Okuyama <rukaokuyama@gmail.com>
2023-05-09 14:33:04 +0900

    minor changes

--- 29 - Countdown Timer/scripts-START.js
+++ 29 - Countdown Timer/scripts-START.js
@@ -4,7 +4,6 @@ const endTime = document.querySelector('.display__end-time');
 const buttons = document.querySelectorAll('[data-time]');
 
 function timer(seconds) {
-  // clear any existing timers
   clearInterval(countdown);
 
   const now = Date.now();
@@ -14,12 +13,10 @@ function timer(seconds) {
 
   countdown = setInterval(() => {
     const secondsLeft = Math.round((then - Date.now()) / 1000);
-    // check if we should stop it!
     if(secondsLeft < 0) {
       clearInterval(countdown);
       return;
     }
-    // display it
     displayTimeLeft(secondsLeft);
   }, 1000);
 }
